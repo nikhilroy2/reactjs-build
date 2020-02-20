923211b82b491292c9a1620c882b2914c4fd0574
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2020-02-20 16:51:43 +0600

    Updates

---
+++ precache-manifest.77f28a1d17ab21ec685625af2dc3d4d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6c89cdedea02579c1cf46abd607c0f",
+    "url": "/reactjs-build/index.html"
+  },
+  {
+    "revision": "e63890b891022b80a313",
+    "url": "/reactjs-build/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e2ae2a1e66e88dadcdd4",
+    "url": "/reactjs-build/static/js/2.4528bc91.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactjs-build/static/js/2.4528bc91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e63890b891022b80a313",
+    "url": "/reactjs-build/static/js/main.6e90a7f5.chunk.js"
+  },
+  {
+    "revision": "936102c1e3f919e4006f",
+    "url": "/reactjs-build/static/js/runtime-main.22191018.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-build/static/media/logo.5d5d9eef.svg"
+  }
+]);
